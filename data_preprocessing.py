dd9641b3ad7564bd49bb24d41f4dc22ea1ac3fe0
Muhammad Ihsan <emhihsan@gmail.com>
2024-07-14 01:56:30 +0700

    add neptune integration

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,6 +3,7 @@ import re
 import nltk
 import string
 import pickle
+import neptune
 import argparse
 import pandas as pd
 from nltk.corpus import stopwords
@@ -10,6 +11,13 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Initialize Neptune
+project_name = os.getenv('NEPTUNE_PROJECT')
+api_token = os.getenv('NEPTUNE_API_TOKEN')
 
 def download_nltk_data():
     nltk.download('punkt')
@@ -27,6 +35,9 @@ def clean_text(text, stop_words, stemmer):
     return text
 
 def preprocess_data(input_file, output_dir):
+
+    run = neptune.init_run(project=project_name, api_token=api_token)
+
     df = pd.read_csv(input_file, encoding='latin1', header=None)
     df = df[[0, 5]]  # Memilih data sentiment dan tweet
     df.columns = ['sentiment', 'tweet']
@@ -66,7 +77,14 @@ def preprocess_data(input_file, output_dir):
     with open(os.path.join(output_dir, 'test_data.pkl'), 'wb') as file:
         pickle.dump(test_data, file)
 
+    # Log preprocessed data and vectorizer to Neptune
+    run['data/preprocessed_data'].upload(os.path.join(output_dir, 'preprocessed_data.csv'))
+    run['data/vectorizer'].upload(os.path.join(output_dir, 'tfidf_vectorizer.pkl'))
+    run['data/train_data'].upload(os.path.join(output_dir, 'train_data.pkl'))
+    run['data/test_data'].upload(os.path.join(output_dir, 'test_data.pkl'))
+
     print("Data preprocessing completed")
+    run.stop()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Preprocess Sentiment140 dataset")
